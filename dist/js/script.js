68b2a1a0ae453f25bc6a1fa5c9d7e53e7ca1001a
Rifqi1012 <rifqimh1210@gmail.com>
2023-11-25 00:35:46 +0700

    portfolio website

---
+++ dist/js/script.js
@@ -0,0 +1,41 @@
+// navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Contact Form
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const url = e.target.action;
+  const formData = new FormData(contactForm);
+
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+      // url Thankyou
+      window.location.href = "/thankyou.html";
+    })
+    .catch((e) => alert("error occured"));
+});
